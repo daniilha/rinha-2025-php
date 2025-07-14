06634c13b088dd3ded4bd2b36f370aafd0b150df
Daniel Ilha <danielilha@furg.br>
2025-07-13 21:43:06 -0300

    formato data ok

--- payments.php
+++ payments.php
@@ -2,7 +2,7 @@
 
 date_default_timezone_set('UTC');
 $now = DateTime::createFromFormat('U.u', microtime(true));
-$d = $now->format('m-d-Y H:i:s.u');
+$d = $now->format('Y-m-d\TH:i:s.u\Z');
 
 $asd = file_get_contents('php://input');
 $payment  = json_decode($asd, true);
@@ -11,7 +11,7 @@ $ch = curl_init('http://payment-processor-default:8080/payments');
 $payload = (file_get_contents('php://input'));
 $payload = json_decode($payload, true);
 $payload['requestedAt']= $d;
-// $payload=json_encode($payload);
+$payload=json_encode($payload);
 curl_setopt($ch, CURLOPT_POSTFIELDS, $payload);
 curl_setopt($ch, CURLOPT_HTTPHEADER, ['Content-Type:application/json']);
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
@@ -20,27 +20,34 @@ curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 1);
 curl_setopt($ch, CURLOPT_TIMEOUT, 400);
 
 $result = curl_exec($ch);
+
+$curl_error = curl_error($ch);
+
+$msg=$result;
+// var_dump($msg);
 curl_close($ch);
 
-// if ($result==false) {
-// 	$processor = 'fallback';
-// 	$ch = curl_init('http://payment-processor-fallback:8080/payments');
-// 	$payload = (file_get_contents('php://input'));
-// 	curl_setopt($ch, CURLOPT_POSTFIELDS, $payload);
-// 	curl_setopt($ch, CURLOPT_HTTPHEADER, ['Content-Type:application/json']);
-// 	curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-// 	$result = curl_exec($ch);
-// 	curl_close($ch);
-// }
+if ($curl_error>0) {
+	$processor = 'fallback';
+	$ch = curl_init('http://payment-processor-fallback:8080/payments');
+	// $payload = (file_get_contents('php://input'));
+	curl_setopt($ch, CURLOPT_POSTFIELDS, $payload);
+	curl_setopt($ch, CURLOPT_HTTPHEADER, ['Content-Type:application/json']);
 
-if ($result!==false) {
-	$msg=json_decode($result, true)['message'];
+	curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 1);
+	curl_setopt($ch, CURLOPT_TIMEOUT, 400);
 
+	// curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+	$result = curl_exec($ch);
+	curl_close($ch);
+}
+
+if (!empty($result)) {
 	$dbconn = pg_connect('host=api-db port=5432 dbname=rinha user=postgres password=postgres');
 	$result = pg_query($dbconn, 'select * from payments');
 
 	$query= "insert INTO payments 
 (correlationId,amount,requested_at,processor) 
-VALUES ('" . $payment['correlationId'] . "'," . $payment['amount'] . ",'" . $d . "','".$processor."')";
+VALUES ('" . $payment['correlationId'] . "'," . $payment['amount'] . ",'" . $d . "','" . $processor . "')";
 	$result = pg_query($dbconn, $query);
 }
