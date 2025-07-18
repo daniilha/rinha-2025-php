55fd1fcd380ce6227de97b45ef2e028d4af91e73
Daniel Ilha <danielilha@furg.br>
2025-07-17 23:01:23 -0300

    ajuste

--- api/daemon.php
+++ api/daemon.php
@@ -7,13 +7,13 @@ while (true) {
 	$dbconn->beginTransaction();
 	$query= "UPDATE payments SET processor = '{$host}', operation = 'busy' WHERE payments.\"correlationId\" IN (select payments.\"correlationId\" as \"correlationId\"	from payments where operation like 'incoming'  
 	ORDER BY requested_at ASC 
-	LIMIT 15 FOR UPDATE)";
+	LIMIT 10 FOR UPDATE)";
 	$result = $dbconn->query($query);
 	$dbconn->commit();
 	$result = $dbconn->query('select payments."correlationId" as "correlationId", amount,requested_at, processor, operation 
 	from payments where operation like \'busy\' AND processor = \'' . $host . '\' 
 	ORDER BY requested_at ASC 
-	LIMIT 30 FOR UPDATE');
+	LIMIT 20');
 	$all = $result->fetchAll();
 	// var_dump($all);
 	if (!empty($all[0])) {
@@ -57,7 +57,7 @@ while (true) {
 		}
 		do {
 			curl_multi_exec($mh, $unfinishedHandles);
-			// usleep(10);
+			usleep(1);
 		} while ($unfinishedHandles);
 		curl_multi_close($mh);
 		$mh2 = curl_multi_init();
@@ -108,7 +108,7 @@ while (true) {
 
 		do {
 			curl_multi_exec($mh2, $unfinishedHandles);
-			// usleep(10);
+			usleep(1);
 		} while ($unfinishedHandles);
 
 		curl_multi_close($mh2);
@@ -117,13 +117,14 @@ while (true) {
 			$handle = $ids[$row['correlationId']]['handle'];
 			$cresult = curl_multi_getcontent($handle);
 			$msg=json_decode($cresult, true);
-
+			// if (!empty($cresult)) {
 			if (!empty($cresult)&&$msg['message']==='payment processed successfully') {
 				// $dbconn = pg_connect('host=api-db port=5432 dbname=rinha user=postgres password=postgres');
 				// $result = pg_query($dbconn, 'select * from payments');
 
 				// if (curl_getinfo($cresult, CURLOPT_URL) == 'http://payment-processor-default:8080/payments') {
 				$processor = $ids[$row['correlationId']]['processor'];
+
 				// } else {
 				// 	$processor = curl_getinfo($handle, CURLINFO_URL);
 				// }
@@ -135,11 +136,14 @@ while (true) {
 				$msg=json_decode($cresult, true);
 				if ($msg['message']!='payment processed successfully') {
 					// echo PHP_EOL, PHP_EOL;
-					print_r("\n" . $row['correlationId']);
+
+					var_dump($all);
+					die;
 				}
 				// sleep(1);
 				// echo "<br />\n";
 				// echo "correlationId: {$row['correlationId']}  amount: {$row['amount']}";
+				// }
 			} else {
 				$query= "UPDATE payments SET operation = 'failed' WHERE payments.\"correlationId\" = '{$row['correlationId']}'";
 				$result = $dbconn->query($query);
